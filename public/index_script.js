6a03b95e9180ca531afdef05be887a55e491a3f5
jimicek09 <jimmy.roberts2009@gmail.com>
2026-06-10 10:45:55 +0200

    working on visuals login page

--- public/index_script.js
+++ public/index_script.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded',
+    function () {
+        const navItems = document
+            .querySelectorAll('.nav-item');
+
+        navItems.forEach(item => {
+            item.addEventListener('click',
+                function () {
+                    navItems.forEach(navItem => navItem
+                        .classList.remove('active'));
+                    this.classList.add('active');
+                });
+        });
+    });
+
 async function loadWords() {
     const response = await fetch('/data');
     const words = await response.json();
